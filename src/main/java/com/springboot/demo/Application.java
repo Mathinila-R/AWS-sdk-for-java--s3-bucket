3910a6eafea42c5e5541e0510992d299ea1b8109
Madhumita R <Madhumita@BCTCL-DL5420534.bctchn.local>
2022-12-05 15:12:00 +0530

    iam user list method is implemented

--- src/main/java/com/springboot/demo/Application.java
+++ src/main/java/com/springboot/demo/Application.java
@@ -1,41 +1,51 @@
 package com.springboot.demo;
 
-import java.util.List;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.Bucket;
-@SpringBootApplication
+import com.amazonaws.services.identitymanagement.AmazonIdentityManagement;
+import com.amazonaws.services.identitymanagement.AmazonIdentityManagementClientBuilder;
+import com.amazonaws.services.identitymanagement.model.ListUsersRequest;
+import com.amazonaws.services.identitymanagement.model.ListUsersResult;
+import com.amazonaws.services.identitymanagement.model.User;
 public class Application {
 
 
 
    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+      
     
         //Creating amazon s3 client
-        final AmazonS3 s3 = AmazonS3ClientBuilder.standard() .build();
+//        final AmazonS3 s3 = AmazonS3ClientBuilder.standard() .build();
         //Listing all the buckets
-        List<Bucket> buckets = s3.listBuckets();
+//        List<Bucket> buckets = s3.listBuckets();
 
 
 
        //Iterating through the bucket
-        buckets.stream().forEach(bucket -> {
-        System.out.println("Bucket Name : " + bucket.getName() + ", "
-                +"\nBucket Owner : " +bucket.getOwner() .getDisplayName ()
-        +",\nBucket CreationDate : " + bucket.getCreationDate());
+//        buckets.stream().forEach(bucket -> {
+//        System.out.println("Bucket Name : " + bucket.getName() + ", "
+//                +"\nBucket Owner : " +bucket.getOwner() .getDisplayName ()
+//        +",\nBucket CreationDate : " + bucket.getCreationDate());
+//
 
 
+        final AmazonIdentityManagement iam =
+            AmazonIdentityManagementClientBuilder.defaultClient();
 
-       });
-    }
+        boolean done = false;
+        ListUsersRequest request = new ListUsersRequest();
 
-   
+        while(!done) {
+            ListUsersResult response = iam.listUsers(request);
+
+            for(User user : response.getUsers()) {
+                System.out.format("Retrieved user %s", user.getUserName());
+            }
+
+            request.setMarker(response.getMarker());
+
+            if(!response.getIsTruncated()) {
+                done = true;
+            }
+        }}}
    
    
    
@@ -81,4 +91,3 @@ public class Application {
    
 
 
-}
